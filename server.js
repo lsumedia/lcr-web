4b2c323597b3f1f83a545fdfac7995337aaa00d4
Cameron Small <camerontsmall@googlemail.com>
2017-10-19 00:05:49 +0100

    basics

---
+++ server.js
@@ -0,0 +1,26 @@
+
+
+/* Load server config */
+
+var config = require('./config.json');
+
+const http = require('http');
+const express = require('express');
+
+
+/* Init server */
+
+var app = express();
+
+var server = http.createServer(app);
+
+app.use(express.static('web'));
+
+if(Number.isInteger(config.port) == true){
+    
+      /* Start server */
+      server.listen(config.port, function () {
+        console.log('Listening on port ' + config.port);
+      });
+    
+    }

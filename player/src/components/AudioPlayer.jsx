e3efd39b1be9877ddf5b7561543bb72bd556c479
Cameron Small <camerontsmall@googlemail.com>
2017-12-07 22:07:13 +0000

    Live player works Take over player to play episode

--- player/src/components/AudioPlayer.jsx
+++ player/src/components/AudioPlayer.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 import newId from '../utils/NewID';
 
+import '../assets/css/AudioPlayer.css';
+
+import { defaultData } from '../Variables.jsx';
+
+/* global $, globals */
+
 function isLive(duration){
     //Switch is in case any browsers are idiots
     switch(duration){
@@ -34,21 +40,45 @@ function niceTime(totalSeconds){
 
 class AudioPlayer extends Component{
 
-    state = {volume: 3}
+
+    streams = [
+        {
+            "url" : "http://ice.lsu.co.uk:8080/lcrhigh",
+            "type" : "audio/mpeg",
+            "title" : "LCR Live"
+        },
+        {
+            "url" : "http://ice.lsu.co.uk:8080/lcr2",
+            "type" : "audio/mpeg",
+            "title" : "LCR Sustain"
+        }
+    ]
+
+    state = {
+        volume: 3,
+        showInfo : false,
+        mode : 0,
+        episodeID: null,
+        episode : {},
+        stream : 0
+    }
 
     constructor(props){
         super(props);
         
-        this.AudioElement = new Audio(this.props.src);
+        globals.AudioPlayer = this;
+
+        this.AudioElement = new Audio();
 
         this.play = this.play.bind(this);
         this.pause = this.pause.bind(this);
         this.reload = this.reload.bind(this);
         this.isPlaying = this.isPlaying.bind(this);
         this.changeVolumeLevel = this.changeVolumeLevel.bind(this);
+        this.toggleInfoBar = this.toggleInfoBar.bind(this);
+        this.goLive = this.goLive.bind(this);
 
         this.AudioElement.addEventListener('playing', () => {
-            console.log(this.AudioElement.duration);
             this.forceUpdate();
         });
 
@@ -73,9 +103,59 @@ class AudioPlayer extends Component{
         this.forceUpdate();
     }
 
+    playEpisode(id){
+        
+        this.setState({
+            mode : 1,
+            episodeID : id
+        }, () => {
+            this.loadAudioFile(true);
+        });
+
+    }
+    
+    goLive(){
+
+        this.setState({
+            mode : 0,
+            episodeID : null
+        }, () => {
+            this.loadAudioFile(true);
+        });
+
+    }
+
+    loadAudioFile(play){
+        var _this = this;
+        if(_this.state.mode == 0){
+            console.log("Load live");
+            var stream = this.streams[this.state.stream];
+            _this.AudioElement.src = stream.url;
+            _this.AudioElement.type = stream.type;
+            _this.AudioElement.load();
+            if(play) this.AudioElement.play();
+        }else{
+            $.getJSON('/api/public/episode/' + this.state.episodeID).done(function(response){
+                console.log("Load episode " + _this.state.episodeID);
+                _this.setState({episode : response});
+                
+                var source = _this.state.episode.meta.media_url[0];
+                _this.AudioElement.src = source.url;
+                _this.AudioElement.type = source.type;
+                _this.AudioElement.load();
+                if(play) _this.AudioElement.play();
+            });
+        }
+    }
+
     isPlaying(){
         if(this.AudioElement.paused) return false;
         return true;
+    }
+
+    toggleInfoBar(){
+        var info = (this.state.showInfo)? false : true;
+        this.setState({showInfo : info});
     }
 
     changeVolumeLevel(){
@@ -87,17 +167,23 @@ class AudioPlayer extends Component{
     }
     
 
+    getContentDetails(){
+        
+        if(this.state.mode == 1){
+            
+            return this.state.episode;
+        }else{
+            return defaultData;
+        }
+
+    }
+
     componentWillMount(){
         this.id = newId("audio");
-       
     }
 
     componentDidMount(){
-        
-        if(this.props.autoplay){
-            this.AudioElement.play();
-        }
-       
+        this.loadAudioFile(this.props.autoplay);
     }
 
     componentWillUnmount(){
@@ -115,18 +201,52 @@ class AudioPlayer extends Component{
         }
         var volume_icon = volume_icons[this.state.volume];
 
+        var showInfo = this.state.showInfo;
+        var menu_icon = (this.state.showInfo)? "keyboard_arrow_down" : "keyboard_arrow_up";
+
+        
+
+        try{
+            var details = this.getContentDetails();
+            var title = details.title;
+            var shortTitle = (title.length > 16)? title.substr(0,13) + '...' : title;
+        }catch(e){
+            title = "";
+            shortTitle = "";
+        }
+
+
         return (
-            <div className="audioplayer">
-                <div className="controls">
-                    <span>
-                        {this.isPlaying() ? 
-                            ( <i className="material-icons" onClick={this.pause} >pause_circle_outline</i>) :
-                            ( <i className="material-icons" onClick={this.play} >play_circle_outline</i>)
-                        }
+            <div>
+                <div className="audioplayer" open={showInfo}>
+                    <div className="controls">
+                        <span>
+                            {this.isPlaying() ? 
+                                ( <i className="material-icons" onClick={this.pause} >pause_circle_outline</i>) :
+                                ( <i className="material-icons" onClick={this.play} >play_circle_outline</i>)
+                            }
+                        </span>
+                        {/*<span>{niceTime(this.AudioElement.duration)}</span> */}
+                        <span className="audioplayer-summary" onClick={this.toggleInfoBar}>
+                            {shortTitle}
+                            <i className="material-icons float-right">{menu_icon}</i>
+                        </span>
+                        <span>
+                            <i className="material-icons float-right" onClick={this.changeVolumeLevel}>{volume_icon}</i>
+                        </span>
+                    </div>
+                </div>
+                <div className="audioplayer-extra ad" open={showInfo}>
+                    <div className="audioplayer-extra-title">
+                        <h4>{title}</h4>
+                        {details.description}
+                    </div>
+                    <p class="audioplayer-additional-controls">
+                        <button className="btn btn-flat" onClick={this.goLive}>Back to LCR Live</button>
                         <i className="material-icons float-right" onClick={this.reload}>skip_next</i>
-                    </span>
-                    {/*<span>{niceTime(this.AudioElement.duration)}</span> */}
-                    <i className="material-icons float-right" onClick={this.changeVolumeLevel}>{volume_icon}</i>
+                    </p>
+                </div>
+                <div className="audioplayer-extra-mask ad" open={showInfo}>
                 </div>
             </div>
         );
--- player/src/views/Episode.jsx
+++ player/src/views/Episode.jsx
@@ -3,7 +3,7 @@ import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
 import { defaultData } from '../Variables.jsx';
 
 //import * as videojs from 'video.js';
-/* global $, videojs */
+/* global $, videojs, globals */
 
 class Episode extends Component{
     
@@ -15,19 +15,32 @@ class Episode extends Component{
         }
     }
 
+    constructor(props){
+        super(props);
+
+        this.playEpisode = this.playEpisode.bind(this);
+    }
+
     getEpisodeData(){
         var id = this.props.match.params.id;
-        console.log("ID IS: " + this.props.match.params.id);
         $.getJSON('/api/public/episode/' + id).done((response) => {
             this.setState({episodeData : response});
         });
     }
+
+    playEpisode(){
+        globals.AudioPlayer.playEpisode(this.props.match.params.id);
+        this.forceUpdate();
+    }
+
+   
 
     componentDidMount(){
         this.getEpisodeData();
     }
 
     render(){
+
 
         var episodeData = this.state.episodeData;
         if(!episodeData.image) episodeData.image = defaultData.image;
@@ -44,10 +57,11 @@ class Episode extends Component{
         <div className="" id="live-container">
           <div className="row">
               <div class="col-sm-12 col-lg-8 col-xl-7 offset-xl-2">
-                <div className="card">
-                  <video controls poster={episodeData.image} className="card-img-top">
-                      {sources}
-                  </video>
+                <div className="card episode-card">
+                    <img className="card-img-top" src={episodeData.image} />
+                    <div className="episode-play-button">
+                        <i className="material-icons" onClick={this.playEpisode}>play_circle_outline</i>
+                    </div>
                 </div>
                 <div className="card-body now-playing-info">
                   <h4 className="card-title">{episodeData.title}</h4>

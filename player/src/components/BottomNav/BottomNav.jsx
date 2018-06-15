3dc0db507885ea8f6d346535c0cd30da0765d31e
Cameron Small <camerontsmall@googlemail.com>
2018-06-15 20:21:08 +0100

    display browse indicator on all browse pages

--- player/src/components/BottomNav/BottomNav.jsx
+++ player/src/components/BottomNav/BottomNav.jsx
@@ -3,6 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 import './BottomNav.css';
 
+const isBrowsing = (match, location) => {
+    console.log(location);
+    if(location.pathname.indexOf('backtrack') != -1) return true;
+    if(location.pathname.indexOf('episode') != -1) return true;
+    if(location.pathname.indexOf('show') != -1) return true;
+}
+
 export default class BottomNav extends Component{
     
     render(){
@@ -15,7 +22,7 @@ export default class BottomNav extends Component{
                         <i className="material-icons">home</i>
                     </NavLink>
 
-                    <NavLink to={'/backtrack'} className="bottom-nav-option" activeClassName="active">
+                    <NavLink to={'/backtrack'} className="bottom-nav-option" isActive={isBrowsing} activeClassName="active">
                         <i className="material-icons">queue_music</i>
                     </NavLink>
 
